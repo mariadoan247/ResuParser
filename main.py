4fc62dc5151aa8cbdd8cd74f86323827208ce93e
mariadoan247 <mariadoan247@ou.edu>
2023-01-24 14:19:44 -0600

    rough draft

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import pandas as pd
+
+resumes = pd.read_csv("UpdatedResumeDataSet.csv")
+
+for i in range(len(resumes)):
+    info = resumes.iloc[i][1]
+    yes = abs(info[i].count('d'))
+    if yes > 0:
+        print(f'{i} is a good resume')
+    else:
+        print(f'{i} is a bad resume')
 
 
-if __name__ == '__main__':
-    print("Hello World! This is ResuParser!")
+# category = input("""
+# Choose Category:
+# 1. Data Science
+# 2. HR
+# """)
+#
+# if category == 1:
+#     print("Data Science")
+# else:
+#     print("hi")
+
+# if __name__ == '__main__':
+#     print("Hello World! This is ResuParser!")
